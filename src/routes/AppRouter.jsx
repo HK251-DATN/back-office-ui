b6efcf42edf74636284926372750c0d9f7a655be
KhoiTranAnh <trananhkhoitv@gmail.com>
2026-07-20 07:47:30 +0700

    add EMPLOYEE role to allowList

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -37,87 +37,87 @@ const AppRouter = () => {
                     <Base />
                 </ProtectedRoute>} >
                 <Route index element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <Dashboard></Dashboard>
                     </ProtectedRoute>
                 } />
                 <Route path="dashboard" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <Dashboard></Dashboard>
                     </ProtectedRoute>
                 } />
                 <Route path="dashboard" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <Dashboard></Dashboard>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-employee" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageEmployee></ManageEmployee>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-customer" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageCustomer></ManageCustomer>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-warehouse" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageWarehouse></ManageWarehouse>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-packaging" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManagePackaging></ManagePackaging>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-shipping" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageShipping></ManageShipping>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-product" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageProduct></ManageProduct>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-category" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageCategory></ManageCategory>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-order" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageOrder></ManageOrder>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-content" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageContent></ManageContent>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-sale-event" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageSaleEvent></ManageSaleEvent>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-coupon" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageCoupon></ManageCoupon>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-provider" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageProvider></ManageProvider>
                     </ProtectedRoute>
                 } />
                 <Route path="manage-raw-product-demand" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <ManageRawProductDemand></ManageRawProductDemand>
                     </ProtectedRoute>
                 } />
                 <Route path="system-setting" element={
-                    <ProtectedRoute allowedRoles={["ADMIN"]}>
+                    <ProtectedRoute allowedRoles={["ADMIN", "EMPLOYEE"]}>
                         <SystemSetting></SystemSetting>
                     </ProtectedRoute>
                 } />
